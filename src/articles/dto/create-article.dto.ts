601d478113607674552bbcca4d08ae4e44609b18
David Ferrell <chrstrock@gmail.com>
2024-07-25 10:52:12 -0400

    updates to dependencies for validation.

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,15 +1,27 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateArticleDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   @ApiProperty()
   title: string;
 
-  @ApiProperty()
+  @ApiProperty({ required: false })
+  @MaxLength(500)
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
   description: string;
 
   @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   body: string;
 
   @ApiProperty({ required: false, default: false })
+  @IsBoolean()
+  @IsOptional()
   published?: boolean = false;
 }
